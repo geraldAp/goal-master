bc854efc1f3fc4253f9f42f8b862258c5c279088
Gerald Amankwah <amankwahprincegerald19@gmail.com>
2024-07-24 16:34:21 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./globals.css";
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
